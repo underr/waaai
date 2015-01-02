27fd3d6733cb8c08a51eb1c6e385636d8f358377
underr <under@insicuri.net>
2015-01-02 04:48:41 -0200

    0.0.4 and private links

--- waaai.js
+++ waaai.js
@@ -5,25 +5,30 @@ var request = require('request');
 var validURl = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
 
 program
-.version('0.0.3')
-.command('*')
-.description('Creates a short link for given URL on waa.ai')
+    .version('0.0.4')
+    .description('link shortener for waa.ai')
+    .option('-p, --private', 'Creates a private link (e.g waa.ai/4gD4/ce42fd.jpg)')
 
-.action(function(env) {
-    if (env.match(validURl)) {
-        request('http://api.waa.ai/shorten?url=' + env, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var akari = JSON.parse(body);
-                var e = akari.data.url
-                console.log(e);
-            } else { 
-                console.log('Error when requesting URL');
-            }
-        })
-    } else {
-        console.log('Not a valid url')
-    }
-});
+program
+    .command('*')
+    .description('Creates a short link for given URL on waa.ai')    
+    .action(function(env, options) {
+        if (program.private) key = '&private=true'
+        if (env.match(validURl)) {
+            request('http://api.waa.ai/shorten?url=' + env + key, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var kyoko = JSON.parse(body);
+                    var akari = kyoko.data.url
+                    console.log(akari);
+                } else { 
+                    console.log('Error when requesting URL');
+                }
+            });
+        } else {
+            console.log('Not a valid url')
+        }
+    });
 
-program.parse(process.argv);
+program.parse(process.argv)
 if (!program.args.length) program.help();
+
