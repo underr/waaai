7fcde37614bf21cee65b7befb5c3edda8e2e24a8
underr <under@insicuri.net>
2015-01-02 04:52:15 -0200

    0.0.5 and undefined key

--- waaai.js
+++ waaai.js
@@ -5,7 +5,7 @@ var request = require('request');
 var validURl = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
 
 program
-    .version('0.0.')
+    .version('0.0.5')
     .description('link shortener for waa.ai')
     .option('-p, --private', 'Creates a private link (e.g waa.ai/4gD4/ce42fd.jpg)')
 
@@ -14,7 +14,11 @@ program
     .description('Creates a short link for given URL on waa.ai')
     .usage('[options] <url>')
     .action(function(env, options) {
-        if (program.private) key = '&private=true'
+        if (program.private) {
+            key = '&private=true'
+        } else {
+            key = ''
+        }
         if (env.match(validURl)) {
             request('http://api.waa.ai/shorten?url=' + env + key, function (error, response, body) {
                 if (!error && response.statusCode === 200) {
