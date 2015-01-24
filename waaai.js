d550423348aea38534b71bef6a8be526b6d73901
underr <under@insicuri.net>
2015-01-24 00:46:32 -0200

    Revert "Semicolons"
    This reverts commit 19425182d53695a71db1770807ddf73f20e67726.

--- waaai.js
+++ waaai.js
@@ -3,41 +3,41 @@ var program = require('commander');
 var request = require('request');
 
 var validURl = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
-var lb = "\n";
+var lb = "\n"
 
 program
   .version('0.2.6')
   .option('-p, --private', 'Create a private link (e.g waa.ai/4gD4/ce42fd.jpg)')
-  .option('-c, --custom <custom URL>', 'Create a custom link (must be between 5 and 30 characters long)', program.custom);
+  .option('-c, --custom <custom URL>', 'Create a custom link (must be between 5 and 30 characters long)', program.custom)
 
 program
   .command('*')
   .description('Create a short link for given URL on waa.ai')
   .action(function(env, options) {
     if (program.private) {
-      key = '&private=true';
+      key = '&private=true'
     } else if (program.custom) {
       key = '&custom=' + program.custom;
     } else {
-      key = '';
+      key = ''
     }
     if (env.match(validURl)) {
       request('http://api.waa.ai/shorten?url=' + env + key, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-          var akari = JSON.parse(body).data.url;
+          var akari = JSON.parse(body).data.url
           console.log(lb + akari + lb);
-        } else {
+        } else { 
           if (program.custom) {
-            console.log('There was a error when requesting URL: custom URL already exists or it is too long/short.');
+            console.log('There was a error when requesting URL: custom URL already exists or it is too long/short.')
           } else {
             console.log('Error when requesting URL');
           }
         }
       });
     } else {
-      console.log('Not a valid url');
+      console.log('Not a valid url')
     }
   });
 
-program.parse(process.argv);
+program.parse(process.argv)
 if (!program.args.length) program.help();
