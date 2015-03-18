f72939680343a620a6bf60fb38765dd18aad4539
underr <under@insicuri.net>
2015-03-17 22:36:14 -0300

    Autism +++

--- cli.js
+++ cli.js
@@ -2,7 +2,6 @@
 var waaai = require('./waaai');
 var argv = require('yargs')
   .usage('Usage: $0 <url> [options]')
-  .demand(1)
   .example('$0 https://github.com/underr/waaai/', 'Creates http://waa.ai/4giF')
   .example('$0 -p http://yuruyuri.com/', 'Creates: http://waa.ai/4gh7/9cf648')
   .example('$0 -c smokeeveryday http://420.moe/', 'Creates: http://waa.ai/smokeeveryday')
@@ -15,17 +14,17 @@ var argv = require('yargs')
 
 if (!argv.c && !argv.p) {
   waaai.link({url: argv._[0]})
-    .then(function (result) {
-      console.log(result)
+    .then(function(result) {
+      console.log(result);
     });
 } else if (argv.c) {
   waaai.link({url: argv._[0], custom: argv.c})
-    .then(function (result) {
-      console.log(result)
+    .then(function(result) {
+      console.log(result);
     });
 } else if (argv.p) {
   waaai.link({url: argv._[0], private: true})
-    .then(function (result) {
-      console.log(result)
+    .then(function(result) {
+      console.log(result);
     });
 }
