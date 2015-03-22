d01321c425912d6272dc94d25c61e560b26ee929
underr <under@insicuri.net>
2015-03-22 18:17:07 -0300

    Avoid Cannot read property 'match' of undefined error

--- cli.js
+++ cli.js
@@ -13,24 +13,28 @@ var argv = require('yargs')
   .argv;
 
 var validURL = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
+var link = argv._[0]
 
-if (!argv._[0].match(validURL)) {
+if (!link) {
+  console.log('You must give a URL.');
+  process.exit(1);
+} else if (!link.match(validURL)) {
   console.log('Not a valid URL.')
   process.exit(1);
 }
 
 if (!argv.c && !argv.p) {
-  waaai.link({url: argv._[0]})
+  waaai.link({url: link})
     .then(function(result) {
       console.log(result);
     });
 } else if (argv.c) {
-  waaai.link({url: argv._[0], custom: argv.c})
+  waaai.link({url: link, custom: argv.c})
     .then(function(result) {
       console.log(result);
     });
 } else if (argv.p) {
-  waaai.link({url: argv._[0], private: true})
+  waaai.link({url: link, private: true})
     .then(function(result) {
       console.log(result);
     });
